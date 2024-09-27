e8e3168e1dbc8323529f5ad0be3455e6a31a6873
ryehwa <rhk415@gmail.com>
2024-09-27 18:14:58 +0900

    Set up multi-module structure

---
+++ services/performance/src/test/java/com/ticketPing/performance/PerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketPing.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
