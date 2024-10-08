da3720824b8811473940d27b3555b0fd6d384541
ryehwa <rhk415@gmail.com>
2024-10-08 12:06:04 +0900

    docker, swagger 및 기본 setting

--- services/payment/src/main/java/com/ticketPing/payment/PaymentApplication.java
+++ services/payment/src/main/java/com/ticketPing/payment/PaymentApplication.java
@@ -2,8 +2,11 @@ package com.ticketPing.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ticketPing.payment", "audit", "exception", "aop"})
 public class PaymentApplication {
 
 	public static void main(String[] args) {
