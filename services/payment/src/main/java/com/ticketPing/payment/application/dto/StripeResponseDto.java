fa3c888395f65488f64d82533182ade45b8f033d
ryehwa <rhk415@gmail.com>
2024-10-10 10:15:18 +0900

    swagger schema 추가

--- services/payment/src/main/java/com/ticketPing/payment/application/dto/StripeResponseDto.java
+++ services/payment/src/main/java/com/ticketPing/payment/application/dto/StripeResponseDto.java
@@ -1,5 +1,6 @@
 package com.ticketPing.payment.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.UUID;
@@ -7,16 +8,21 @@ import java.util.UUID;
 @Getter
 @Setter
 public class StripeResponseDto {
+    @Schema(description = "Stripe paymentIntentId")
     private String paymentIntentId;
     private UUID orderId;
     private String userEmail;
+    @Schema(description = "payment status", example = "canceled, processing, requires_action, requires_capture, requires_confirmation," +
+            "requires_payment_method,succeeded")
     private String status;
     private Long amount;
     //private String currency;
+    @Schema(description = "for frontend key")
     private String clientSecret;
     private String performanceName;
     private String performanceTime;
     private String seatInfo;
+    @Schema(description = "payment Intent Time _ unix timestamp", example = "1680800504")
     private Long paymentIntentTime;
 
 
--- services/payment/src/main/java/com/ticketPing/payment/presentation/request/StripeRequestDto.java
+++ services/payment/src/main/java/com/ticketPing/payment/presentation/request/StripeRequestDto.java
@@ -1,5 +1,6 @@
 package com.ticketPing.payment.presentation.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,10 @@ import java.util.UUID;
 public class StripeRequestDto {
 
     private String performanceName;
+    @Schema(description = "Time of the performance", example = "2024-12-24T20:00:00")
     private String performanceTime;
     private String seatInfo;
+    @Schema(description = "price")
     private Long amount;
     private String userEmail;
 
