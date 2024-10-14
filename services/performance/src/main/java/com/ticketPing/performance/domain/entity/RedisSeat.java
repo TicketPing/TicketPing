359722466d265ace0fec1d4f1742201632d37b83
mii2026 <mii2026@skku.edu>
2024-10-14 20:50:58 +0900

    [feat #90] 전체 좌석 캐싱 생성 api 추가

---
+++ services/performance/src/main/java/com/ticketPing/performance/domain/entity/RedisSeat.java
@@ -0,0 +1,34 @@
+package com.ticketPing.performance.domain.entity;
+
+import com.ticketPing.performance.domain.entity.enums.SeatRate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.UUID;
+
+@Data
+@Builder(access = AccessLevel.PRIVATE)
+@RedisHash("seat")
+public class RedisSeat {
+    @Id
+    private UUID seatId;
+    private Integer row;
+    private Integer col;
+    private Boolean seatState;
+    private SeatRate seatRate;
+    private Integer cost;
+
+    public static RedisSeat from(Seat seat) {
+        return RedisSeat.builder()
+                .seatId(seat.getId())
+                .row(seat.getRow())
+                .col(seat.getCol())
+                .seatState(seat.getSeatState())
+                .seatRate(seat.getSeatCosts().getSeatRate())
+                .cost(seat.getSeatCosts().getCost())
+                .build();
+    }
+}
