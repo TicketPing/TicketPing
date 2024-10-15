a245b742cfc4cd1e2da7d201a521c5a7c533f5f9
ryehwa <rhk415@gmail.com>
2024-10-15 21:34:11 +0900

    modify webflux config

---
+++ gateway/src/main/java/com/ticketping/gateway/config/WebFluxConfig.java
@@ -0,0 +1,19 @@
+package com.ticketping.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
