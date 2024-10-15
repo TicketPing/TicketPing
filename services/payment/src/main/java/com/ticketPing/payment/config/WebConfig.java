960f4a84e169d0ac66d5e0e8a1e224724d5d3b88
ryehwa <rhk415@gmail.com>
2024-10-15 20:37:46 +0900

    [fix] 배포 관련 수정

--- services/payment/src/main/java/com/ticketPing/payment/config/WebConfig.java
+++ services/payment/src/main/java/com/ticketPing/payment/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React 앱 URL
+                        .allowedOrigins("*") // React 앱 URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(false);
