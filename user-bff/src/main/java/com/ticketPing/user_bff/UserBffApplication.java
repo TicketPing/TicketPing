7cf060c04b27c7afae616d8ed7bd66213a3e0dd9
mii2026 <mii2026@skku.edu>
2024-10-20 04:28:56 +0900

    [feat #131] user-bff 서비스 추가

---
+++ user-bff/src/main/java/com/ticketPing/user_bff/UserBffApplication.java
@@ -0,0 +1,13 @@
+package com.ticketPing.user_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBffApplication.class, args);
+	}
+
+}
---
+++ user-bff/src/test/java/com/ticketPing/user_bff/UserBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketPing.user_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
